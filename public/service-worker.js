8012f715956262cccc6a5da26a2eca6118009356
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-04-26 18:43:54 -0500

    fixed event request URL and cache files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache"
+const STATIC_CACHE = "static-cache-v2";
+const RUNTIME_CACHE = "data-cache-v1"
 
 const FILES_TO_CACHE = [
     "/",
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
      "/icons/icon-512x512.png"
   ];
   
-  self.addEventListener('install', (event) => {
+  self.addEventListener('install', function(event) {
     event.waitUntil(
       caches
         .open(STATIC_CACHE)
@@ -22,7 +22,7 @@ const FILES_TO_CACHE = [
   });
   
   // The activate handler takes care of cleaning up old caches.
-  self.addEventListener('activate', (event) => {
+  self.addEventListener('activate', function(event) {
     const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
     event.waitUntil(
       caches
@@ -41,7 +41,7 @@ const FILES_TO_CACHE = [
     );
   });
   
-  self.addEventListener("fetch", event => {
+  self.addEventListener("fetch", function(event) {
     // non GET requests are not cached and requests to other origins are not cached
     if (
       event.request.method !== "GET" ||
@@ -49,20 +49,24 @@ const FILES_TO_CACHE = [
     ) {
       event.respondWith(fetch(event.request));
       return;
-    }
+    };
   
     // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/images")) {
+    if (event.request.url.includes("/api/transaction")) {
       // make network request and fallback to cache if network request fails (offline)
       event.respondWith(
         caches.open(RUNTIME_CACHE).then(cache => {
           return fetch(event.request)
             .then(response => {
               cache.put(event.request, response.clone());
+              if (response.status === 200) {
+                cache.put(event.request, response.clone());
+              };
               return response;
             })
             .catch(() => caches.match(event.request));
         })
+        .catch(err => console.log(err))
       );
       return;
     }
